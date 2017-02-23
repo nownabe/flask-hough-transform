4a1049a5b05abeca9be5beaeab4d8362b14a607d
nownabe <nownabe@goodpatch.com>
2017-02-23 13:58:18 +0900

    :city_sunrise: Upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import datetime
 from flask import Flask
+from flask import request
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 app.config.update({'DEBUG': True })
@@ -11,6 +14,13 @@ def index():
 @app.route("/hello")
 def hello():
     return "Hello, world!"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    image = request.files["image"]
+    filename = "images/" + datetime.date.today().strftime("%Y%m%d%H%M%S") + "_" + image.filename
+    image.save("./static/" + filename)
+    return url_for("static", filename=filename)
 
 if __name__ == "__main__":
     app.run()
