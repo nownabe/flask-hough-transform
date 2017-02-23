a5baecfd9525b607f651ea9f20ef8cacad57c6b7
nownabe <nownabe@goodpatch.com>
2017-02-23 14:53:10 +0900

    :star2: Show uploaded image

--- app.py
+++ app.py
@@ -18,9 +18,10 @@ def hello():
 @app.route("/upload", methods=["POST"])
 def upload():
     image = request.files["image"]
-    filename = "images/" + datetime.date.today().strftime("%Y%m%d%H%M%S") + "_" + image.filename
-    image.save("./static/" + filename)
-    return url_for("static", filename=filename)
+    filename = "./static/images/" + datetime.date.today().strftime("%Y%m%d%H%M%S") + "_" + image.filename
+    image.save(filename)
+
+    return filename
 
 if __name__ == "__main__":
     app.run()
