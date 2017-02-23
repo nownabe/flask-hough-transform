afc05ef8d66a599d877e891666bf46cb5a732a70
nownabe <nownabe@goodpatch.com>
2017-02-23 11:02:37 +0900

    :newspaper: Introduce template

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello")
 def hello():
     return "Hello, world!"
 
